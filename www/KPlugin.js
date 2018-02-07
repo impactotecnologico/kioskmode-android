518cd45c6a6cc43735e18a9ef198157c3d4ec027
Impacto Tecnologico <josejulian@impactotecnologico.net>
2018-02-07 18:06:59 +0100

    improve en ampliacion de alcance de plugin

--- www/KPlugin.js
+++ www/KPlugin.js
@@ -1,41 +1,42 @@
+cordova.define("net.impactotecnologico.kioskmode.KPlugin", function(require, exports, module) {
+  var exec = require('cordova/exec');
 
-var exec = require('cordova/exec');
+  var KActivity = {
+      
+      exitKiosk: function () {
+          exec(null, null, "KPlugin", "exitKiosk", []);
+      },
+      
+      isInKiosk: function (callback) {
+          if(/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
+              callback(false); // ios not supported - cannot be in kiosk
+              return;
+          }
+          exec(function(out){
+              callback(out == "true");
+          }, function(error){
+              alert("KPlugin.isInKiosk failed: " + error);
+          }, "KPlugin", "isInKiosk", []);
+      },
+      
+      isSetAsLauncher: function (callback) {
+          if(/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
+              callback(false); // ios not supported - cannot be in kiosk
+              return;
+          }
+          exec(function(out){
+              callback(out == "true");
+          }, function(error){
+              alert("KPlugin.isSetAsLauncher failed: " + error);
+          }, "KPlugin", "isSetAsLauncher", []);
+      },
+      
+      setAllowedKeys: function (keyCodes) {
+          exec(null, null, "KPlugin", "setAllowedKeys", keyCodes);
+      }
+      
+  }
 
-var KActivity = {
-    
-    exitKiosk: function () {
-        exec(null, null, "KPlugin", "exitKiosk", []);
-    },
-    
-    isInKiosk: function (callback) {
-        if(/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
-            callback(false); // ios not supported - cannot be in kiosk
-            return;
-        }
-        exec(function(out){
-            callback(out == "true");
-        }, function(error){
-            alert("KPlugin.isInKiosk failed: " + error);
-        }, "KPlugin", "isInKiosk", []);
-    },
-    
-    isSetAsLauncher: function (callback) {
-        if(/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
-            callback(false); // ios not supported - cannot be in kiosk
-            return;
-        }
-        exec(function(out){
-            callback(out == "true");
-        }, function(error){
-            alert("KPlugin.isSetAsLauncher failed: " + error);
-        }, "KPlugin", "isSetAsLauncher", []);
-    },
-    
-    setAllowedKeys: function (keyCodes) {
-        exec(null, null, "KPlugin", "setAllowedKeys", keyCodes);
-    }
-    
+  module.exports = KActivity;
+
 }
-
-module.exports = KActivity;
-
