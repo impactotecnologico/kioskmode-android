54f8a3a052c9f6532cf9099026b52c4cafa3d801
Impacto Tecnologico <josejulian@impactotecnologico.net>
2018-02-07 16:17:54 +0100

    inclusion de clase plugin cordova

--- src/android/java/net/impactotecnologico/kioskmode/KPlugin.java
+++ src/android/java/net/impactotecnologico/kioskmode/KPlugin.java
@@ -1,115 +1,40 @@
-package net.impactotecnologico.kioskmode;
+package net.it.arit.plugin;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class KPlugin extends ActionBarActivity {
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
 
-    private final List blockedKeys = new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP));
-    private Button hiddenExitButton;
-    private int currentApiVersion;
-    public static String CLASS_TAG=KPlugin.class.getSimpleName();
+public class KPlugin extends CordovaPlugin {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-        setContentView(R.layout.activity_main);
+  private final String TAG = "KPlugin";
+  private Activity activity;
+  private Context context;
 
-        // every time someone enters the kiosk mode, set the flag true
-        PrefUtils.setKioskModeActive(true, getApplicationContext());
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
 
-        hiddenExitButton = (Button) findViewById(R.id.hiddenExitButton);
-        hiddenExitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Break out!
-                PrefUtils.setKioskModeActive(false, getApplicationContext());
-                Toast.makeText(getApplicationContext(),"Ahora si puedes salir de la app!", Toast.LENGTH_SHORT).show();
-            }
-        });
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    this.context = cordova.getActivity().getApplicationContext();
+    this.activity = cordova.getActivity();
+    this.action = action;
+    try {
+      Intent intent = new Intent(context, KioskActivity.class);
+      System.out.println("Entra en vista");
+      this.cordova.getActivity().startActivity(intent);
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        hideNavigationBar1();
-        Log.d(CLASS_TAG,"Resume");
-    }
-
-    private void hideNavigationBar1() {
-        currentApiVersion = Build.VERSION.SDK_INT;
-
-        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-        // This work only for android 4.4+
-        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                getWindow().getDecorView().setSystemUiVisibility(flags);
-            }
-
-            // Code below is to handle presses of Volume up or Volume down.
-            // Without this, after pressing volume buttons, the navigation bar will
-            // show up and won't hide
-            final View decorView = getWindow().getDecorView();
-            decorView
-                    .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-
-                        @Override
-                        public void onSystemUiVisibilityChange(int visibility) {
-                            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                                decorView.setSystemUiVisibility(flags);
-                            }
-                        }
-                    });
-        }
-
-
-
-
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if(!hasFocus) {
-            // Close every kind of system dialog
-            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-            sendBroadcast(closeDialog);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        // nothing to do here
-        // … really
-    }
-
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        if (blockedKeys.contains(event.getKeyCode())) {
-            return true;
-        } else {
-            return super.dispatchKeyEvent(event);
-        }
-    }
-
+    return true;
+  }
 }
